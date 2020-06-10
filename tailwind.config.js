4c36dc752c07a28ceba9bbb92667b463c2b8a2eb
Ulysse Tallepied <ulysse.tallepied@hetic.net>
2020-06-10 16:34:11 +0200

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  purge: [
+    "./src/**/*.html",
+    "./src/**/*.js",
+    "./src/**/*.jsx",
+    "./src/**/*.tsx",
+    "./src/**/*.ts",
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
