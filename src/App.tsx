25ebc5215842c8a8c0ebde5ded0ee3e0b4deb323
Ulysse Tallepied <ulysse.tallepied@hetic.net>
2020-06-10 18:41:47 +0200

    Test deploy

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="text-center">
       <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-4xl text-white">
-        <p>Mémoire numérique</p>
+        <p>Mémoire numérique?</p>
       </header>
     </div>
   );
