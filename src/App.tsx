39a6aa05f3d6a13d23c3b9818f05acad3a238072
Ulysse Tallepied <ulysse.tallepied@hetic.net>
2020-06-10 18:57:43 +0200

    Add license

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,36 @@
 import React from "react";
 import "./styles/main.css";
+import ReactFullpage from "@fullpage/react-fullpage";
 
 function App() {
   return (
-    <div className="text-center">
-      <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-4xl text-white">
-        <p>Mémoire numérique?</p>
-      </header>
-    </div>
+    <ReactFullpage
+      //fullpage options
+      licenseKey={"YOUR_KEY_HERE"}
+      scrollingSpeed={1000} /* Options here */
+      render={({ state, fullpageApi }) => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="section">
+              <p>Section 1 (welcome to fullpage.js)</p>
+              <button onClick={() => fullpageApi.moveSectionDown()}>
+                Click me to move down
+              </button>
+            </div>
+            <div className="section">
+              <p>Section 2</p>
+            </div>
+            <div className="section">
+              <div className="text-center">
+                <header className="bg-gray-900 min-h-screen flex flex-col items-center justify-center text-4xl text-white">
+                  <p>Mémoire numérique?</p>
+                </header>
+              </div>
+            </div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
   );
 }
 
